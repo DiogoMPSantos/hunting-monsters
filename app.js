78077e50364b34bb1b28ebaa690388ad86e55270
Diogo Santos <diogo.santtosicet@gmail.com>
2021-11-08 23:40:49 -0300

    create structure and layout panel and score divs

---
+++ app.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: "#app",
+    data: {
+        running: false,
+        playerLife: 0,
+        monsterLife: 100
+    },
+    computed: {
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
+    },
+    methods: {
+        startGame(){
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        },
+
+        giveUp(){
+            this.running = false
+        }
+    }
+})
