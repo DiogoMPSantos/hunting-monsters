7f66f77cc87e374fff10dc7722e379b17d0c398f
Diogo Santos <diogo.santtosicet@gmail.com>
2021-11-13 10:37:21 -0300

    Add function attack and heal

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ new Vue({
     el: "#app",
     data: {
         running: false,
-        playerLife: 0,
-        monsterLife: 100
+        playerLife: 100,
+        monsterLife: 100,
+        logs: []
     },
     computed: {
         hasResult(){
@@ -17,8 +18,49 @@ new Vue({
             this.monsterLife = 100
         },
 
+        attack(special){
+            this.specialAttack('playerLife', 5, 10, special, 'Player', 'Monster', 'player');
+            if (this.monsterLife > 0) {
+                this.specialAttack('monsterLife', 7, 12, special, 'Monster', 'Player', 'monster');   
+            }
+        },
+
+        specialAttack(player, min , max, special, source, target, style) {
+            const plus =special ? 5 : 0;
+            const damage = this.gettRandom(min + plus, max + plus);
+            this[player] = Math.max(this[player] - damage, 0);
+            this.registerLog(`${source} atingiu ${target} com ${damage}.`, style);
+        },
+
+        healAndHurt(){
+            this.heal(10, 15);
+            this.specialAttack('playerLife', 7, 12, false, 'Monster', 'Player', 'monster');
+        },
+
+        heal(min, max){
+            const heal = Math.random(min, max)
+            this.playerLife = Math.max(this.playerLife + heal, 100);
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player');
+        },
+
+        gettRandom(min, max){
+            const value = Math.random() * (max - min) + min;
+            return Math.round(value);
+        },
+
         giveUp(){
             this.running = false
+        },
+
+        registerLog(text, style){
+            this.logs.unshift({text, style});
+        }
+    },
+    watch: {
+        hasResult(value){
+            if (value) {
+                this.running = false;
+            }
         }
     }
 })
